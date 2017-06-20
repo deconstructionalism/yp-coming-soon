8ebe2ac3937c664e7f382d9ecfb3a91c914a1d68
GreenPenguinDev <arjun@gpenguin.com>
2017-06-20 18:14:09 -0400

    added changing background animation, date+time

--- src/js/preLoad.js
+++ src/js/preLoad.js
@@ -1,3 +1,26 @@
 window.onload = function() {
     document.querySelector('#loading-overlay').classList.add('fadeout')
-}
+};
+
+
+(function() {
+
+    var backgrounds = [
+        'images/giant-kitty-bg.jpg',
+        'images/haram-bg.jpg',
+        'images/ravi-shavi-bg.jpg'
+    ];
+
+    var bodyEl = document.querySelector('body');
+    bodyEl.style.backgroundImage = 'url(' + backgrounds[0] + ')';
+
+    var counter = 1;
+    var range = backgrounds.length;
+
+    setInterval(function() {
+        counter = counter === range ? 0 : counter;
+        var bgImg = backgrounds[counter];
+        bodyEl.style.backgroundImage = 'url(' + bgImg + ')';
+        counter += 1;
+    }, 3000);
+})();
